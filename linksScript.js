47697b829fcf39392321b583248b7635c3ec6f5d
levtus <61996958+levtus@users.noreply.github.com>
2023-05-01 16:28:18 -0500

    Add new sections

--- linksScript.js
+++
@@ -1,1 +0,0 @@
-$("#iFrameId").contents().find(".fixed left-16 bottom-[52px] -m-1 hidden items-center space-x-1 rounded-[12px] p-1 transition-colors xl:flex 2xl:space-x-2 bg-transparent duration-[0]").remove();
