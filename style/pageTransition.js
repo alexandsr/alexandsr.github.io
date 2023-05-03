0feb6c1a4ac1048f9ba72b973deb1b5bfa0263a9
levtus <61996958+levtus@users.noreply.github.com>
2023-05-03 09:30:04 -0500

    Add page transitions.

---
+++ style/pageTransition.js
@@ -0,0 +1,17 @@
+// Code stolen from dris.llc
+
+function toProjects() {
+    document.getElementById("squeegee").style.height = "100vh";
+    setTimeout( function() { window.location.href = '⌨' }, 600);
+
+};
+
+function toResume() {
+    document.getElementById("squeegee").style.height = "100vh";
+    setTimeout( function() { window.location.href = '🗐' }, 600);
+};
+
+function toInfo() {
+    document.getElementById("squeegee").style.height = "100vh";
+    setTimeout( function() { window.location.href = '🛈' }, 600);
+};
